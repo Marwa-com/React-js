69fe06c0511d88c33e7fa2a1fb70c0cf32ff415a
Marwa <amri.maroia@yahoo.fr>
2021-04-28 23:57:19 +0100

    adding code

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FacebookLoginButton } from "react-social-login-buttons";
 function App() {
   return (
     <form className="App">
+      <header>
         <h2> LOGIN IN </h2>
-        <h4> Welcome </h4>
+        <h3> Welcome </h3>
         <Form.Group controlId="formBasicEmail">      
     <Form.Label >Email </Form.Label>
     <Form.Control type="email" placeholder="email" />   
@@ -22,14 +24,18 @@ function App() {
   <Button variant="primary" type="submit">
     log in
   </Button>
+  <p> Or continue with your social acount </p>
+  <FacebookLoginButton className="facebook" />
   <Container>
   <Row>
     <Col className="c1" > <Button>Forget password</Button></Col>
     <Col className="c2"  ><Button>LOGIN UP</Button></Col>
+    <Col className="c3"  ><h4> Contact us : +216 777777777</h4></Col>
   </Row>
    </Container>
+   </header>
     </form>
- 
+    
   );
 }
 
